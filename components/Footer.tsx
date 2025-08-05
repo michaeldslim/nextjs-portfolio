60f25c31e6d8edaaaa547f107782e9469911924c
Mike Lim <michaelds.lim@gmail.com>
2025-08-05 14:15:13 -0500

    refactor: simplify footer by removing FreeDNS link and adjusting layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,21 +1,10 @@
-import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between">
-      <div className="mb-2">
-        <Link
-          href="https://freedns.afraid.org/"
-          target="_blank"
-          className="text-gray-500"
-          style={{ fontSize: "10px" }}
-        >
-          Free DNS{" "}
-        </Link>
-      </div>
+    <div className="flex justify-end">
       <div>
-        <span className="text-gray-500" style={{ fontSize: "10px" }}>
+        <span className="text-gray-500 text-right" style={{ fontSize: "10px" }}>
           Next.js 13 + React + TypeScript + Tailwind CSS
         </span>
       </div>
